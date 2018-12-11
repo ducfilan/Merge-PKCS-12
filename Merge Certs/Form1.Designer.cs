305a9c27ffecaab5e2b6c08e95f7ac41bcad4acf
ducfilan <ducfilan@gmail.com>
2018-12-11 13:56:00 +0900

    First commit

---
+++ Merge Certs/Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace Merge_Certs
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.txtCertFiles = new System.Windows.Forms.RichTextBox();
+            this.btnSelectCertFiles = new System.Windows.Forms.Button();
+            this.btnCombineCertFiles = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnBrowseOutputFile = new System.Windows.Forms.Button();
+            this.txtOutputFile = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.saveFileDialog = new System.Windows.Forms.SaveFileDialog();
+            this.SuspendLayout();
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.Filter = "Cert files|*.pfx;*.p12";
+            this.openFileDialog.Multiselect = true;
+            this.openFileDialog.Title = "Select your certs";
+            // 
+            // txtCertFiles
+            // 
+            this.txtCertFiles.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.txtCertFiles.Location = new System.Drawing.Point(12, 21);
+            this.txtCertFiles.Name = "txtCertFiles";
+            this.txtCertFiles.ReadOnly = true;
+            this.txtCertFiles.Size = new System.Drawing.Size(695, 275);
+            this.txtCertFiles.TabIndex = 0;
+            this.txtCertFiles.Text = "";
+            // 
+            // btnSelectCertFiles
+            // 
+            this.btnSelectCertFiles.Location = new System.Drawing.Point(713, 19);
+            this.btnSelectCertFiles.Name = "btnSelectCertFiles";
+            this.btnSelectCertFiles.Size = new System.Drawing.Size(75, 23);
+            this.btnSelectCertFiles.TabIndex = 1;
+            this.btnSelectCertFiles.Text = "Browse";
+            this.btnSelectCertFiles.UseVisualStyleBackColor = true;
+            this.btnSelectCertFiles.Click += new System.EventHandler(this.btnSelectCertFiles_Click);
+            // 
+            // btnCombineCertFiles
+            // 
+            this.btnCombineCertFiles.Location = new System.Drawing.Point(277, 364);
+            this.btnCombineCertFiles.Name = "btnCombineCertFiles";
+            this.btnCombineCertFiles.Size = new System.Drawing.Size(168, 74);
+            this.btnCombineCertFiles.TabIndex = 2;
+            this.btnCombineCertFiles.Text = "Combine certs";
+            this.btnCombineCertFiles.UseVisualStyleBackColor = true;
+            this.btnCombineCertFiles.Click += new System.EventHandler(this.btnCombineCertFiles_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 6);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 12);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Cert files";
+            // 
+            // btnBrowseOutputFile
+            // 
+            this.btnBrowseOutputFile.Location = new System.Drawing.Point(713, 327);
+            this.btnBrowseOutputFile.Name = "btnBrowseOutputFile";
+            this.btnBrowseOutputFile.Size = new System.Drawing.Size(75, 23);
+            this.btnBrowseOutputFile.TabIndex = 4;
+            this.btnBrowseOutputFile.Text = "Browse";
+            this.btnBrowseOutputFile.UseVisualStyleBackColor = true;
+            this.btnBrowseOutputFile.Click += new System.EventHandler(this.btnBrowseOutputFile_Click);
+            // 
+            // txtOutputFile
+            // 
+            this.txtOutputFile.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.txtOutputFile.Location = new System.Drawing.Point(12, 329);
+            this.txtOutputFile.Name = "txtOutputFile";
+            this.txtOutputFile.ReadOnly = true;
+            this.txtOutputFile.Size = new System.Drawing.Size(695, 19);
+            this.txtOutputFile.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 314);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 12);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Output file";
+            // 
+            // saveFileDialog
+            // 
+            this.saveFileDialog.Filter = "Cert files|*.pfx;*.p12";
+            this.saveFileDialog.Title = "Specify your file to save";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtOutputFile);
+            this.Controls.Add(this.btnBrowseOutputFile);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnCombineCertFiles);
+            this.Controls.Add(this.btnSelectCertFiles);
+            this.Controls.Add(this.txtCertFiles);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Merge Certs";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.RichTextBox txtCertFiles;
+        private System.Windows.Forms.Button btnSelectCertFiles;
+        private System.Windows.Forms.Button btnCombineCertFiles;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnBrowseOutputFile;
+        private System.Windows.Forms.TextBox txtOutputFile;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog;
+    }
+}
+
---
+++ Merge Certs/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Specialized;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Windows.Forms;
+
+namespace Merge_Certs
+{
+    public partial class Form1 : Form
+    {
+        private const string CertPassword = "fuji";
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            txtCertFiles.Text = string.Join("\r\n", Properties.Settings.Default.SourceCertFilesPath.Cast<string>().ToArray());
+            txtOutputFile.Text = Properties.Settings.Default.TargetCertFilePath;
+        }
+
+        private void btnSelectCertFiles_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog.ShowDialog() != DialogResult.OK) return;
+
+            txtCertFiles.Text = string.Join("\r\n", openFileDialog.FileNames);
+
+            var sourceCertFilesPath = new StringCollection();
+            sourceCertFilesPath.AddRange(openFileDialog.FileNames);
+            Properties.Settings.Default.SourceCertFilesPath = sourceCertFilesPath;
+            Properties.Settings.Default.Save();
+        }
+
+        private void btnCombineCertFiles_Click(object sender, EventArgs e)
+        {
+            var certs = new X509Certificate2Collection();
+
+            foreach (var certFile in txtCertFiles.Text.Split("\r\n".ToCharArray()))
+            {
+                certs.Add(new X509Certificate2(certFile, CertPassword, X509KeyStorageFlags.Exportable));
+            }
+
+            var oneBigPfx = certs.Export(X509ContentType.Pfx, CertPassword);
+            File.WriteAllBytes(txtOutputFile.Text, oneBigPfx);
+
+            MessageBox.Show("Combination has been done!", "Done", MessageBoxButtons.OK);
+        }
+
+        private void btnBrowseOutputFile_Click(object sender, EventArgs e)
+        {
+            if (saveFileDialog.ShowDialog() != DialogResult.OK) return;
+
+            txtOutputFile.Text = saveFileDialog.FileName;
+            Properties.Settings.Default.TargetCertFilePath = txtOutputFile.Text;
+            Properties.Settings.Default.Save();
+        }
+    }
+}
